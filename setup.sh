f8c99794a076894c810cd4ab9fe699874786460d
Rajkale99 <kaleraj.rk@gmail.com>
2025-04-13 03:01:31 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Install some packages
 if [[ -e "/usr/bin/apt-get" ]]; then
-    sudo apt-get install -y aria2 arj brotli cabextract cmake device-tree-compiler gcc g++ git liblz4-tool liblzma-dev libtinyxml2-dev lz4 mpack openjdk-11-jdk p7zip-full p7zip-rar python3 python3-pip rar sharutils unace unrar unzip uudeview xz-utils zip zlib1g-dev
+    sudo apt-get install -y aria2 arj brotli cabextract cmake device-tree-compiler gcc g++ git liblz4-tool liblzma-dev libtinyxml2-dev lz4 mpack openjdk-11-jdk p7zip-full p7zip-rar python3 python3-pip rar sharutils unace unrar unzip uudeview xz-utils zip zlib1g-dev python3-protobuf build-essential libssl-dev libffi-dev python3-dev
 elif [[ -e "/usr/bin/pacman" ]]; then
     sudo pacman -Syu --needed --noconfirm android-tools aria2 arj brotli cabextract cmake dtc gcc git lz4 xz tinyxml2 p7zip python-pip unrar sharutils unace zip unzip uudeview zip
 elif [[ -e "/usr/bin/yum" ]]; then
