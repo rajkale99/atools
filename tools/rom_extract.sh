06c820b68d674fc95e29b6caf60c1a1adaacddd7
Rajkale99 <kaleraj.rk@gmail.com>
2025-04-13 02:57:49 +0530

    Update rom_extract.sh

--- tools/rom_extract.sh
+++ tools/rom_extract.sh
@@ -79,10 +79,10 @@ for var in "$@"; do
             echo -e "Mounting & copying ${DIR_NAME}"
             mkdir -p $PROJECT_DIR/dumps/${UNZIP_DIR}/$DIR_NAME $PROJECT_DIR/dumps/$UNZIP_DIR/tempmount
             # mount & permissions
-            sudo -S mount -o loop "$PROJECT_DIR/dumps/${UNZIP_DIR}/$file.img" "$PROJECT_DIR/dumps/${UNZIP_DIR}/tempmount"
-            sudo -S chown -R $USER:$USER "$PROJECT_DIR/dumps/${UNZIP_DIR}/tempmount"
-            sudo -S chmod -R u+rwX "$PROJECT_DIR/dumps/${UNZIP_DIR}/tempmount"
-            sudo -S chmod -R 777 "$PROJECT_DIR/dumps/${UNZIP_DIR}/tempmount"
+            sudo -S mount -o ro "$PROJECT_DIR/dumps/${UNZIP_DIR}/$file.img" "$PROJECT_DIR/dumps/$UNZIP_DIR/$file/
+            sudo -S chown -R $USER:$USER "$PROJECT_DIR/dumps/${UNZIP_DIR}/$file"
+            sudo -S chmod -R u+rwX "$PROJECT_DIR/dumps/${UNZIP_DIR}/$file"
+            sudo -S chmod -R 777 "$PROJECT_DIR/dumps/${UNZIP_DIR}/$file"
             
             # copy to dump
             sudo cp -a $PROJECT_DIR/dumps/${UNZIP_DIR}/tempmount/* $PROJECT_DIR/dumps/$UNZIP_DIR/$DIR_NAME
